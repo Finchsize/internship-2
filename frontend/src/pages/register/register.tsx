e2f9d9cd8f5d2a911d7d61ae4714f7c02e970ba9
Marcel Alefierowicz <mrclalefierowicz@gmail.com>
2022-08-03 12:41:41 +0200

    WN2-9: - added account creating capability to the site

--- frontend/src/pages/register/register.tsx
+++ frontend/src/pages/register/register.tsx
@@ -9,14 +9,53 @@ import {
   FormErrorMessage,
   FormHelperText,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 export const Register = () => {
-
+  const [login, setLogin] = useState("");
   const [email, setEmail] = useState<string>();
-  const [pass, setPass] = useState<string>();
-  const [secondaryPass, setSecondaryPass] = useState<string>();
-  
+  const [pass, setPass] = useState<string>(); // reduntant
+  const [secondaryPass, setSecondaryPass] = useState<string>(); // reduntant
+  const [exception, setException] = useState("");
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await axios({
+      method: "POST",
+      url: "http://localhost:8080/users",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "*/*",
+      },
+      data: {
+        nickname: login,
+        firstName: "string",
+        lastName: "string",
+        email: email,
+        phoneNumber: "123456789",
+        country: "String",
+        city: "Ghymobuimaoebegdraagbwgzwmbzajmmjtwxieocvqpskubhrmqgpernlomzhw",
+        language: "POLISH",
+      },
+      responseType: "json",
+    }).then((response) => {
+      console.log(response);
+      Cookies.set("token", response.data.token, {expires: 7});
+
+    }).catch(function(error){
+      if(error.response){
+        setException("");
+      }
+      else if (error.request) {
+        console.log(error.request);
+      }
+      else {
+        console.log("Error", error.message);
+      }
+    })
+  };
 
   function checkEmail(): boolean {
     if (typeof email === "undefined") {
@@ -25,21 +64,18 @@ export const Register = () => {
     return /\S+@\S+\.\S+/.test(email);
   }
   const checkPassLength = (): boolean => {
-
     if (typeof pass === "undefined") return true;
     else return pass.length > 5;
-  }
-  function passwordValidation(e:any){
-  
+  };
+
+  function passwordValidation(e: any) {
+    // reduntant
     setPass(e);
-    
-    
   }
-  const confirmPass = ():boolean =>{
-    
+  const confirmPass = (): boolean => {
+    // redundant
     return pass === secondaryPass;
-    
-  }
+  };
 
   return (
     <div>
@@ -67,52 +103,74 @@ export const Register = () => {
             </Heading>
 
             <Box width={"100%"}>
-              <FormControl my={8}>
-                <FormLabel fontSize={"xl"}>
-                  {" "}
-                  <ChatIcon> </ChatIcon> Name
-                </FormLabel>
-                <Input type="text" max={30} mb={"2rem"} />
+              <form onSubmit={handleSubmit}>
+                <FormControl my={8}>
+                  <FormLabel fontSize={"xl"}>
+                    {" "}
+                    <ChatIcon> </ChatIcon> Name
+                  </FormLabel>
+                  <Input
+                    value={login}
+                    type="text"
+                    onChange={(e) => setLogin(e.target.value)}
+                    mb={"2rem"}
+                  />
 
-                <FormLabel fontSize={"xl"}>
-                  {" "}
-                  <EmailIcon> </EmailIcon> Email
-                </FormLabel>
-                <Input
-                  type="email"
-                  mb={"2rem"}
-                  isInvalid={!checkEmail()}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
+                  <FormLabel fontSize={"xl"}>
+                    {" "}
+                    <EmailIcon> </EmailIcon> Email
+                  </FormLabel>
+                  <Input
+                    type="email"
+                    value={email}
+                    mb={"2rem"}
+                    isInvalid={!checkEmail()}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
 
-                <FormLabel fontSize={"xl"}>
-                  {" "}
-                  <LockIcon> </LockIcon> Password
-                </FormLabel>
-                <Input type="password" isInvalid={!checkPassLength()} onChange={(e) => (passwordValidation(e.target.value))}/>
-                <FormHelperText mb={"2rem"}>
-                  Password has to contain atleast 6 characters.
-                </FormHelperText>
+                  <FormLabel fontSize={"xl"}>
+                    {" "}
+                    <LockIcon> </LockIcon> <s>Password</s>
+                  </FormLabel>
+                  <Input
+                    isDisabled={true}
+                    type="password"
+                    isInvalid={!checkPassLength()}
+                    onChange={(e) => passwordValidation(e.target.value)}
+                  />
+                  <FormHelperText mb={"2rem"}>
+                    Password has to contain atleast 6 characters.
+                  </FormHelperText>
 
-                <FormLabel fontSize={"xl"}>
-                  {" "}
-                  <LockIcon> </LockIcon> Confirm Password
-                </FormLabel>
-                <Input isInvalid={!confirmPass()} onChange={(e) => setSecondaryPass(e.target.value)} type="password" mb={"3rem"} />
-              </FormControl>
+                  <FormLabel fontSize={"xl"}>
+                    {" "}
+                    <s>
+                      {" "}
+                      <LockIcon> </LockIcon> Confirm Password
+                    </s>
+                  </FormLabel>
+                  <Input
+                    isDisabled={true}
+                    isInvalid={!confirmPass()}
+                    onChange={(e) => setSecondaryPass(e.target.value)}
+                    type="password"
+                    mb={"3rem"}
+                  />
 
-              <Flex
-                width={"full"}
-                alignItems={"center"}
-                justifyContent={"space-between"}
-              >
-                <Button colorScheme="blue" variant={"link"}>
-                  Don't have an account?
-                </Button>
-                <Button colorScheme="teal" type={"submit"}>
-                  Sign in
-                </Button>
-              </Flex>
+                  <Flex
+                    width={"full"}
+                    alignItems={"center"}
+                    justifyContent={"space-between"}
+                  >
+                    <Button colorScheme="blue" variant={"link"}>
+                      Don't have an account?
+                    </Button>
+                    <Button colorScheme="teal" type={"submit"}>
+                      Sign in
+                    </Button>
+                  </Flex>
+                </FormControl>
+              </form>
             </Box>
           </Stack>
         </Flex>
