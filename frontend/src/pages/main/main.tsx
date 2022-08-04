2e8954ab51bd5ae657a5551ceb871a9212953ce6
Maciej Malinowski <105872023+mejsiejdev@users.noreply.github.com>
2022-08-04 13:50:06 +0200

    WN2-15
    - added `@chakra-ui/icons`
    - refactor `main` to use new components

--- frontend/src/pages/main/main.tsx
+++ frontend/src/pages/main/main.tsx
@@ -1,14 +1,53 @@
-import { Outlet } from "react-router";
-import { Link } from "react-router-dom";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Avatar,
+  AvatarBadge,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  IconButton,
+  Input,
+  List,
+  ListItem,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { BellIcon, ChatIcon, SettingsIcon } from "@chakra-ui/icons";
+
+import { Sidebar } from "../../components/Sidebar";
+import { Topbar } from "../../components/Topbar";
+import { ChatDetails } from "../../components/ChatDetails";
 
 export const Main = () => {
   return (
-    <div>
-      <h1>Clean template.</h1>
-      <nav>
-        <Link to="/signin">Sign in</Link> | <Link to="/register">Register</Link>
-      </nav>
-      <Outlet />
-    </div>
+    <Flex
+      flexDirection={"row"}
+      bgColor={"blackAlpha.50"}
+      w={"full"}
+      h={"100vh"}
+    >
+      <Sidebar />
+      <VStack w="full" h="full" spacing={"0"}>
+        <Topbar />
+        <HStack w="full" h="full" alignItems={"flex-end"}>
+          <Flex
+            alignItems={"flex-start"}
+            justifyContent={"flex-end"}
+            flexDirection={"column"}
+            padding={"1rem"}
+            w="full"
+          >
+            <Input placeholder="Message..." bgColor={"white"} shadow={"sm"} />
+          </Flex>
+          <ChatDetails />
+        </HStack>
+      </VStack>
+    </Flex>
   );
 };
