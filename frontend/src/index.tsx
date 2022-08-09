8ce347bbbf54f0786f9ffbc4f55568e0ab8db40e
Maciej Malinowski <105872023+mejsiejdev@users.noreply.github.com>
2022-08-09 09:33:04 +0200

    WN2-32
    - added `i18n`
    - created `i18n.ts`

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./i18n";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Suspense>
+        <App />
+      </Suspense>
     </ChakraProvider>
   </React.StrictMode>
 );
