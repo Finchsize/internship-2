408026509e62c6e0c3b50ad5130a734628f47b11
Maciej Malinowski <105872023+mejsiejdev@users.noreply.github.com>
2022-08-19 13:53:12 +0200

    WN2-80
    added necessery functionalities for effective management of channel's members

--- frontend/src/components/MemberAdd/MemberAdd.tsx
+++ frontend/src/components/MemberAdd/MemberAdd.tsx
@@ -16,26 +16,78 @@ import {
   HStack,
   IconButton,
   Input,
+  VStack,
 } from "@chakra-ui/react";
+import Cookies from "js-cookie";
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import axiosInstance from "../../lib/axios";
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
+  channel: number;
 };
 
 type Inputs = {
   nick: string;
 };
 
-export const MemberAdd = ({ isOpen, onClose }: Props) => {
+export const MemberAdd = ({ isOpen, onClose, channel }: Props) => {
   const { register, handleSubmit } = useForm<Inputs>();
   const [searching, setSearching] = useState(false);
   const [successful, setSuccessful] = useState<boolean>();
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState("");
+
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    console.log(data);
+    setSearching(true);
+    const nick = data.nick.trim();
+    if (nick !== "") {
+      await axiosInstance({
+        method: "get",
+        url: `/users/${nick}`,
+        headers: {
+          Authorization: Cookies.get("token")!,
+        },
+      })
+        .then((res) => {
+          setUser(res.data.nickname);
+          setSuccessful(true);
+        })
+        .catch((error) => {
+          if (error.response) {
+            setSuccessful(false);
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log("Error", error.message);
+          }
+        });
+    }
+    setSearching(false);
   };
+
+  const addToChannel = async (asOwner: boolean) => {
+    setLoading(true);
+    await axiosInstance({
+      method: "put",
+      url: "channels/users",
+      headers: {
+        Authorization: Cookies.get("token")!,
+      },
+      data: {
+        id: channel,
+        userNickname: user,
+        action: asOwner ? "ADD_OWNER" : "ADD_MEMBER",
+        directMessage: false,
+      },
+    }).then(() => {
+      setLoading(false);
+      onClose();
+    });
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
@@ -64,40 +116,39 @@ export const MemberAdd = ({ isOpen, onClose }: Props) => {
                     <FormHelperText>User found!</FormHelperText>
                   ))}
               </FormControl>
-              {searching ? (
-                <CircularProgress isIndeterminate size={"1.75rem"} />
-              ) : successful !== true ? (
+              {successful !== true && (
                 <IconButton
+                  isLoading={searching}
                   type={"submit"}
                   icon={<SearchIcon />}
-                  aria-label={"Search for user"}
-                />
-              ) : (
-                <IconButton
-                  colorScheme={"blue"}
-                  variant={"solid"}
-                  type={"submit"}
-                  icon={<AddIcon />}
                   aria-label={"Search for user"}
                 />
               )}
             </HStack>
           </form>
         </ModalBody>
-        <ModalFooter w={"full"}>
-          {successful && (
-            <>
-              <Text>Do you want to add user as a member or as an owner?</Text>
-              <HStack w={"full"} gap={4}>
-                <Button w={"full"} variant="outline">
+        <ModalFooter w={"full"} gap={4} alignItems="center" justifyItems={"center"}>
+          {successful &&
+            (!loading ? (
+              <>
+                <Button
+                  w={"full"}
+                  variant="outline"
+                  onClick={() => addToChannel(false)}
+                >
                   Add as a member
                 </Button>
-                <Button w={"full"} colorScheme="blue">
+                <Button
+                  w={"full"}
+                  colorScheme="blue"
+                  onClick={() => addToChannel(true)}
+                >
                   Add as an owner
                 </Button>
-              </HStack>
-            </>
-          )}
+              </>
+            ) : (
+              <CircularProgress isIndeterminate size={"1.75rem"} />
+            ))}
         </ModalFooter>
       </ModalContent>
     </Modal>
--- frontend/src/components/MemberManage/MemberManage.tsx
+++ frontend/src/components/MemberManage/MemberManage.tsx
@@ -7,37 +7,97 @@ import {
   ModalBody,
   ModalFooter,
   Button,
-  Text,
 } from "@chakra-ui/react";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../lib/axios";
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
+  channel: number;
   member: {
     nickname: string;
     owner: boolean;
   };
 };
 
-export const MemberManage = ({ isOpen, onClose, member }: Props) => {
+export const MemberManage = ({ isOpen, onClose, channel, member }: Props) => {
+  const [removing, setRemoving] = useState(false);
+  const [promoting, setPromoting] = useState(false);
+
+  const changeRole = async () => {
+    setPromoting(true);
+    await axiosInstance({
+      method: "put",
+      url: "channels/users",
+      headers: {
+        Authorization: Cookies.get("token")!,
+      },
+      data: {
+        id: channel,
+        userNickname: member.nickname,
+        action: member.owner ? "REMOVE_OWNER" : "ADD_OWNER",
+        directMessage: false,
+      },
+    }).then(() => {
+      setPromoting(false);
+      onClose();
+    });
+  };
+
+  const removeMember = async () => {
+    setRemoving(true);
+    await axiosInstance({
+      method: "put",
+      url: "channels/users",
+      headers: {
+        Authorization: Cookies.get("token")!,
+      },
+      data: {
+        id: channel,
+        userNickname: member.nickname,
+        action: "REMOVE_MEMBER",
+        directMessage: false,
+      },
+    }).then(() => {
+      setRemoving(false);
+      onClose();
+    });
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Manage user</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          {`What do you want to with ${member.nickname}?`}
-        </ModalBody>
+        <ModalCloseButton disabled={removing || promoting} onClick={onClose} />
+        <ModalBody>{`What do you want to with ${member.nickname}?`}</ModalBody>
         <ModalFooter w={"full"} gap={4}>
-          <Button w={"full"} variant="outline">
+          <Button
+            disabled={removing}
+            isLoading={promoting}
+            loadingText={"Adding ownership..."}
+            w={"full"}
+            variant="outline"
+            onClick={changeRole}
+          >
             {`Make ${member.nickname} ${
               member.owner ? " a member" : " an owner"
             }`}
           </Button>
-          <Button w={"full"} colorScheme="red">
-            {`Remove ${member.nickname}`}
-          </Button>
+          {!member.owner && (
+            <Button
+              w={"full"}
+              disabled={promoting}
+              isLoading={removing}
+              loadingText={"Removing..."}
+              colorScheme="red"
+              onClick={removeMember}
+            >
+              {`Remove ${member.nickname}`}
+            </Button>
+          )}
         </ModalFooter>
       </ModalContent>
     </Modal>
